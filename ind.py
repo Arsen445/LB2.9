01d1c175508f4560545071f0f6021a392fb5d877
Arsen445 <100753942+Arsen445@users.noreply.github.com>
2022-11-01 12:10:18 +0300

    Update ind.py

--- ind.py
+++ ind.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+def akkerman(m, n):
+    if m == 0 and n > 0:
+        return n + 1
+    elif m > 0 and n == 0:
+        return akkerman(m - 1, 1)
+    elif m > 0 and n > 0:
+        return akkerman(m - 1, akkerman(m, n - 1))
+    else:
+        return None
 
 if __name__ == '__main__':
-    def akkerman(m, n):
-        if m == 0 and n > 0:
-            return n + 1
-        elif m > 0 and n == 0:
-            return akkerman(m - 1, 1)
-        elif m > 0 and n > 0:
-            return akkerman(m - 1, akkerman(m, n - 1))
-        else:
-            return None
 
     print(akkerman(2, 3))
       
